abcf7bf45747be4cb2caedf6446a48976b3bd95a
Nicholas Powers <njpowers7915@gmail.com>
2018-05-26 22:30:58 +0000

    Added list_shows and show_details methods which will initially display hardcoded data

--- resident_advisor_event_finder/lib/resident_advisor_event_finder/cli.rb
+++ resident_advisor_event_finder/lib/resident_advisor_event_finder/cli.rb
@@ -5,6 +5,19 @@ class ResidentAdvisorEventFinder::CLI
   def call
     puts "Welcome to the 'Resident Advisor' Event Finder"
     puts "Please enter the name of a US state where you would like to see a show: "
+    list_shows
+    show_details
   end
+  
+  #list_shows will list all upcoming shows in a particular state indicated by user_input 
+  def list_shows
+    input = nil
+    when input != "exit"
+      puts "Upcoming shows in #{input}"
+  end
+  
+  #show_details will display details of particular show
+  def show_details
+    
 
 end
