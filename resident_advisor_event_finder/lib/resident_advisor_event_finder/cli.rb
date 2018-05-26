f25f7d7eb0fd12420dc6aedbf0153be5bbdfd93e
Nicholas Powers <njpowers7915@gmail.com>
2018-05-26 23:04:23 +0000

    continued building out list_shows and show_details

--- resident_advisor_event_finder/lib/resident_advisor_event_finder/cli.rb
+++ resident_advisor_event_finder/lib/resident_advisor_event_finder/cli.rb
@@ -3,8 +3,7 @@
 class ResidentAdvisorEventFinder::CLI
   
   def call
-    puts "Welcome to the 'Resident Advisor' Event Finder"
-    puts "Please enter the name of a US state where you would like to see a show: "
+    puts "Welcome to the 'Resident Advisor' Event Finder!"
     list_shows
     show_details
   end
@@ -12,12 +11,33 @@ class ResidentAdvisorEventFinder::CLI
   #list_shows will list all upcoming shows in a particular state indicated by user_input 
   def list_shows
     input = nil
+    puts "Please enter the name of a US state where you would like to see a show: "
+    input = gets.strip.downcase
     when input != "exit"
       puts "Upcoming shows in #{input}"
+      puts <<-DOC.gsub /^\s*/,
+        1. Midnight Lovers x Making Shapes with Move D - May 26 - Los Angeles
+        2. Better Listen Records with Martin Miguel - May 26 - Los Angeles
+      DOC
   end
   
   #show_details will display details of particular show
   def show_details
+    input = nil
+    until input = "exit"
+      puts "Enter the number of the show you'd like to learn more about"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Name"
+        puts "Date"
+        puts "City"
+        puts "Venue"
+        puts "Event Link"
+        puts "Link to Tickets"
+      end
+    end
+  end
     
 
 end
